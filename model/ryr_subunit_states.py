3ea599a37e294ef7fbaca9de1afb60b685c05dbd
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2024-06-25 15:40:52 -0500

    Generate states and reactions automatically

---
+++ model/ryr_subunit_states.py
@@ -0,0 +1,81 @@
+import itertools
+from lxml import etree
+
+kfs = {"CaM": 2.4e-7, "CaMCa2C": 3.15e-7, "CaMCa4": 3.66e-7, "2CaC": 6e-5,
+       "2CaN":  0.1e-2}
+
+
+krs = {"CaM": 2.4e-7, "CaMCa2C": 3.67e-6, "CaMCa4": 1.28e-6, "2CaC": 9.1e-3,
+       "2CaN": 1000e-3}
+counter = 1
+
+def add_reaction(root, name, what, new_name):
+    global counter
+    my_r = etree.SubElement(root, "Reaction",
+                            name=name+"_"+what+"_"+str(counter),
+                            id=name+"_"+what+"_"+str(counter))
+    etree.SubElement(my_r, "Reactant", specieID=name)
+    if what != "2CaC" and what != "2CaN":
+        etree.SubElement(my_r, "Reactant", specieID=what)
+    else:
+        etree.SubElement(my_r, "Reactant", specieID="Ca", n="2")
+    etree.SubElement(my_r, "Product", specieID=new_name)
+    kf = etree.SubElement(my_r, "forwardRate")
+    kf.text = str(kfs[what])
+    kr = etree.SubElement(my_r, "reverseRate")
+    kr.text = str(krs[what])
+    q = etree.SubElement(my_r, "Q10")
+    q.text = ".2"
+    counter += 1
+
+    
+def generate_name(a, b, c):
+    name = "RyR"
+    if a:
+        name += "_%dCaM" % a
+    if b:
+        name += "_%dCaMCa2C" % b
+    if c:
+        name += "_%dCaMCa4" % c
+    return name
+
+if __name__ == "__main__":
+    states = set()
+    for i in range(0, 5):
+        for j in range(0, 5):
+            for k in range(0, 5):
+                    if i+j+k > 4:
+                        continue
+                    for iteration in itertools.permutations((i,j,k),r=3):
+                        states.add(iteration)
+
+    my_rxn_file = etree.Element("ReactionScheme")
+    for (i,  j, k) in sorted(states):
+        my_specie_name = generate_name(i, j, k)
+        etree.SubElement(my_rxn_file, "Specie", name=my_specie_name,
+                         id=my_specie_name, kdiff="0", kdiffunit="mu2/s")
+
+    
+    for (i,  j, k) in sorted(states):
+        my_specie_name = generate_name(i, j, k)
+    
+        if i+j+k < 4:
+            add_reaction(my_rxn_file, my_specie_name, "CaM",
+                         generate_name(i+1,j, k))
+            add_reaction(my_rxn_file, my_specie_name, "CaMCa2C",
+                         generate_name(i,j+1, k))
+            add_reaction(my_rxn_file, my_specie_name, "CaMCa4",
+                         generate_name(i,j, k+1))
+        else:
+            pass   
+        if not i:
+            pass 
+        else:
+            add_reaction(my_rxn_file, my_specie_name, "2CaC",
+                         generate_name(i-1, j+1, k))
+        if not j:    
+            pass  
+        else:
+            add_reaction(my_rxn_file, my_specie_name, "2CaN",
+                         generate_name(i, j-1, k+1))
+    print(etree.tostring(my_rxn_file, pretty_print=True).decode("utf-8"))
