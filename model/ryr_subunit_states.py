2086dd359fa5a1e6ce3e72fad3f34e37db2ed3f6
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2024-06-27 16:11:37 -0500

    Fit to Zahradnikova according to her experimental methods

--- model/ryr_subunit_states.py
+++ model/ryr_subunit_states.py
@@ -3,15 +3,15 @@ from lxml import etree
 
 fname = "Rxn_module_RyR2_CaM.xml"
 
-kfs = {"CaM": 2.4e-8, #2.1e-8 for Kd of 820 nM
+kfs = {"CaM": 2.4e-7, #2.1e-8 for Kd of 820 nM
        "CaMCa2C": 3.15e-7, "CaMCa4": 3.66e-7, "2CaC": 6e-5,
-       "2CaN":  0.1e-2, "RyRCa1": 8.84e-3, "RyRCa2": 2.88e-3,
-       "RyRCa3":0.00381, "O":0.43}
+       "2CaN":  0.1e-2, "RyRCa1": 3.04e-3, "RyRCa2": 5.76e-3,
+       "RyRCa3":0.00615, "O": 9.912, "C": 0.75}
 
 
 krs = {"CaM": 1.73e-5, "CaMCa2C": 3.67e-6, "CaMCa4": 1.28e-6, "2CaC": 9.1e-3,
-       "2CaN": 1000e-3,"RyRCa1": 8.84e-2, "RyRCa2": 2.88e-2, "RyRCa3":0.152,
-       "O": 7.43}
+       "2CaN": 1000e-3,"RyRCa1": 3.04e-2, "RyRCa2": 5.76e-2, "RyRCa3":0.246,
+       "O": 1.945, "C": 0.114}
 counter = 1
 
 def add_reaction(root, name, what, new_name):
@@ -20,11 +20,11 @@ def add_reaction(root, name, what, new_name):
                             name=name+"_"+what+"_"+str(counter),
                             id=name+"_"+what+"_"+str(counter))
     etree.SubElement(my_r, "Reactant", specieID=name)
-    if what != "2CaC" and what != "2CaN" and not what.startswith("RyR") and what != "O":
+    if what != "2CaC" and what != "2CaN" and not what.startswith("RyR") and what not in ["O", "C"]:
         etree.SubElement(my_r, "Reactant", specieID=what)
     elif what == "RyRCa1" or what == "RyRCa2":
         etree.SubElement(my_r, "Reactant", specieID="Ca")
-    elif what == "O":
+    elif what in ["O", "C"]:
         pass
     else:
         etree.SubElement(my_r, "Reactant", specieID="Ca", n="2")
@@ -138,13 +138,21 @@ if __name__ == "__main__":
                              new_name)
 
     for i, specie in enumerate(ryr_species_to_open):
-        etree.SubElement(my_rxn_file, "Specie", name="O%d" % i,
-                         id="O%d" % i, kdiff="0", kdiffunit="mu2/s")
+        etree.SubElement(my_rxn_file, "Specie", name="%sO" % specie,
+                         id="%sO" % specie, kdiff="0", kdiffunit="mu2/s")
 
     for i, specie in enumerate(ryr_species_to_open):
         add_reaction(my_rxn_file, specie, "O",
-                     "O%d"%i)
-        
+                     "%sO"%specie)
+
+    for i, specie in enumerate(ryr_species_to_open):
+        etree.SubElement(my_rxn_file, "Specie", name="%sC" % specie,
+                         id="%sC" % specie, kdiff="0", kdiffunit="mu2/s")
+
+    for i, specie in enumerate(ryr_species_to_open):
+        add_reaction(my_rxn_file, "%sO" % specie, "C",
+                     "%sC"%specie)
+
                 
     f = open(fname, "w")
     f.write(etree.tostring(my_rxn_file, pretty_print=True).decode("utf-8"))
