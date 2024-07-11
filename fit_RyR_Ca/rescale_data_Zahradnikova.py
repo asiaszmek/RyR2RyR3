61a474d7fe6688f8ada6edb7692823fb4b7d7aa2
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2024-07-11 15:13:07 +0200

    Stim starts at 0.5 ms

--- fit_RyR_Ca/rescale_data_Zahradnikova.py
+++ fit_RyR_Ca/rescale_data_Zahradnikova.py
@@ -8,7 +8,7 @@ files = glob.glob(os.path.join(directory, "po_?0_uM_Ca.csv"))
 
 for fname in files:
     data = np.loadtxt(fname, skiprows=1, delimiter=',')
-    data[np.where(data[:, 1] < 0)] = 0
+    data[:, 0] += 0.5
     data[:, 1] = data[:, 1]*35
     new_fname = "po" + fname.split("po")[-1]
     np.savetxt(new_fname, data, header="time,RO", delimiter=",")
