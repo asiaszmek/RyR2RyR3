821b52b24fbef3980253f6c2ab2b8fb616fdec63
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2024-07-24 11:32:56 +0200

    Add clarity

--- model/ryr2_subunit_states.py
+++ model/ryr2_subunit_states.py
@@ -5,24 +5,24 @@ fname = "Rxn_module_RyR2_CaM.xml"
 
 kfs = {"CaM": 2.1e-8, # for Kd of 820 nM (Xu and Meissner 2004 for RyR2)
        "CaMCa2C": 3.15e-7, "CaMCa4": 3.66e-7, "2CaC": 6e-5,
-       "2CaN":  0.1e-2, "RyRCa1": 1e-3, "RyRCa2": 0.75e-3,
-       "RyRCa3":5e-4, "RyRCa4": 2.5e-4, "RyRCa4O1": 38.4, "RyRCa4O1C1": 0.0025,
-       "RyRCa4O2": 38.4e-3, "RyRCa4O2C1":2.5, "RyRCa4C1I": 11.28,
-       "CaMRyRCa1": 1e-3, "CaMRyRCa2": 0.75e-3,
-       "CaMRyRCa3":5e-4, "CaMRyRCa4": 2.5e-4, "CaMRyRCa4O1": 5.21,
-       "CaMRyRCa4O1C1": 0.16,
-       "CaMRyRCa4O2": 5.21e-3, "CaMRyRCa4O2C1":0.16e3, "CaMRyRCa4C1I": 4.37}
+       "2CaN":  0.1e-2, "RyR2Ca1": 1e-3, "RyR2Ca2": 0.75e-3,
+       "RyR2Ca3":5e-4, "RyR2Ca4": 2.5e-4, "RyR2Ca4O1": 38.4, "RyR2Ca4O1C1": 0.0025,
+       "RyR2Ca4O2": 38.4e-3, "RyR2Ca4O2C1":2.5, "RyR2Ca4C1I": 11.28,
+       "CaMRyR2Ca1": 1e-3, "CaMRyR2Ca2": 0.75e-3,
+       "CaMRyR2Ca3":5e-4, "CaMRyR2Ca4": 2.5e-4, "CaMRyR2Ca4O1": 5.21,
+       "CaMRyR2Ca4O1C1": 0.16,
+       "CaMRyR2Ca4O2": 5.21e-3, "CaMRyR2Ca4O2C1":0.16e3, "CaMRyR2Ca4C1I": 4.37}
 
 
 krs = {"CaM": 1.73e-5, "CaMCa2C": 3.67e-6, "CaMCa4": 1.28e-6,
-       "2CaC": 9.1e-3, "2CaN": 1000e-3,"RyRCa1": 1,
-       "RyRCa2": 2, "RyRCa3": 3, "RyRCa4": 4,
-       "RyRCa4O1": 3,"RyRCa4O1C1": 0.77, "RyRCa4O2": 3e-3,
-       "RyRCa4O2C1": 0.77e3,  "RyRCa4C1I":0.05,
-       "CaMRyRCa1": 1,
-       "CaMRyRCa2": 2, "CaMRyRCa3": 3, "CaMRyRCa4": 4,
-       "CaMRyRCa4O1": 8.08,"CaMRyRCa4O1C1": 32, "CaMRyRCa4O2": 8.08e-3,
-       "CaMRyRCa4O2C1": 32e3,  "CaMRyRCa4C1I":0.95}
+       "2CaC": 9.1e-3, "2CaN": 1000e-3,"RyR2Ca1": 1,
+       "RyR2Ca2": 2, "RyR2Ca3": 3, "RyR2Ca4": 4,
+       "RyR2Ca4O1": 3,"RyR2Ca4O1C1": 0.77, "RyR2Ca4O2": 3e-3,
+       "RyR2Ca4O2C1": 0.77e3,  "RyR2Ca4C1I":0.05,
+       "CaMRyR2Ca1": 1,
+       "CaMRyR2Ca2": 2, "CaMRyR2Ca3": 3, "CaMRyR2Ca4": 4,
+       "CaMRyR2Ca4O1": 8.08,"CaMRyR2Ca4O1C1": 32, "CaMRyR2Ca4O2": 8.08e-3,
+       "CaMRyR2Ca4O2C1": 32e3,  "CaMRyR2Ca4C1I":0.95}
 counter = 1
 
 def add_reaction(root, name, what, new_name):
@@ -51,9 +51,9 @@ def add_reaction(root, name, what, new_name):
     
 def generate_name(a, b, c, d=0):
     if not d:
-        name = "RyR"
+        name = "RyR2"
     else:
-        name = "Ca%d_RyR" % d
+        name = "Ca%d_RyR2" % d
     if a:
         name += "_%dCaM" % a
     if b:
@@ -145,9 +145,9 @@ if __name__ == "__main__":
            
             new_name = generate_name(i, j, k, l + 1)
             if "CaM" in my_specie_name:
-                rxn_name = "CaMRyRCa%d" % (l+1)
+                rxn_name = "CaMRyR2Ca%d" % (l+1)
             else:
-                rxn_name = "RyRCa%d" % (l+1)
+                rxn_name = "RyR2Ca%d" % (l+1)
             add_reaction(my_rxn_file, my_specie_name, rxn_name,
                          new_name)
 
@@ -165,26 +165,26 @@ if __name__ == "__main__":
 
     for i, specie in enumerate(ryr_species_to_open):
         if "CaM" not in specie:
-            add_reaction(my_rxn_file, specie, "RyRCa4O1",
+            add_reaction(my_rxn_file, specie, "RyR2Ca4O1",
                          "%s_O1"%specie)
-            add_reaction(my_rxn_file, specie, "RyRCa4O2",
+            add_reaction(my_rxn_file, specie, "RyR2Ca4O2",
                          "%s_O2"%specie)
-            add_reaction(my_rxn_file, "%s_O1" % specie,"RyRCa4O2C1",
+            add_reaction(my_rxn_file, "%s_O1" % specie,"RyR2Ca4O2C1",
                          "%s_C1"%specie)
-            add_reaction(my_rxn_file, "%s_O2" % specie,"RyRCa4O2C1",
+            add_reaction(my_rxn_file, "%s_O2" % specie,"RyR2Ca4O2C1",
                          "%s_C1"%specie)
-            add_reaction(my_rxn_file, "%s_C1" % specie,"RyRCa4C1I",
+            add_reaction(my_rxn_file, "%s_C1" % specie,"RyR2Ca4C1I",
                          "%s_I"%specie)
         else:
-            add_reaction(my_rxn_file, specie, "CaMRyRCa4O1",
+            add_reaction(my_rxn_file, specie, "CaMRyR2Ca4O1",
                          "%s_O1"%specie)
-            add_reaction(my_rxn_file, specie, "CaMRyRCa4O2",
+            add_reaction(my_rxn_file, specie, "CaMRyR2Ca4O2",
                          "%s_O2"%specie)
-            add_reaction(my_rxn_file, "%s_O1" % specie,"CaMRyRCa4O2C1",
+            add_reaction(my_rxn_file, "%s_O1" % specie,"CaMRyR2Ca4O2C1",
                          "%s_C1"%specie)
-            add_reaction(my_rxn_file, "%s_O2" % specie,"CaMRyRCa4O2C1",
+            add_reaction(my_rxn_file, "%s_O2" % specie,"CaMRyR2Ca4O2C1",
                          "%s_C1"%specie)
-            add_reaction(my_rxn_file, "%s_C1" % specie,"CaMRyRCa4C1I",
+            add_reaction(my_rxn_file, "%s_C1" % specie,"CaMRyR2Ca4C1I",
                          "%s_I"%specie)
 
                 
