eec433f5090354e63a325f07e81c25a7e9a9e9a3
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2025-10-03 09:02:44 +0200

    First working version
    (based on the MCell4 examples)

--- MCell/ryr2.py
+++ MCell/ryr2.py
@@ -1,173 +1,151 @@
 import sys                                  #Have to code for multiple seeds,have to add custom cylindrical camkii_rel and just run for diff ca,diff configuration of camkii??
 import os
 import random
+import copy
+import numpy as np
+
+
+# set recursively region to all nodes of type m.ExprNodeType.LEAF
+def set_region_to_all_count_term_leaf_nodes(count_term_node, region):
+    if not count_term_node:
+        return
+
+    if count_term_node.node_type != m.ExprNodeType.LEAF:
+        set_region_to_all_count_term_leaf_nodes(count_term_node.left_node, region)
+        set_region_to_all_count_term_leaf_nodes(count_term_node.right_node, region)
+    else:
+        count_term_node.region = region
+
+
+def load_bngl_observables_and_create_psd_and_spine_variants(model, file_name, spine, psd, sampling_periodicity, seed):
+
+    # create a helper object just to load the observables from BNGL,
+    observables = m.Observables()
+    observables.load_bngl_observables(file_name)
+
+    for base_count in observables.counts:
+        # count in psd
+        count_psd = copy.deepcopy(base_count)
+        count_psd.name = str(count_psd.name + '_psd') 
+        count_psd.file_name = str('./react_data/seed_' + str(seed).zfill(5) + '/' + count_psd.name + '.dat')
+        count_psd.every_n_timesteps = sampling_periodicity
+        set_region_to_all_count_term_leaf_nodes(count_psd.expression, psd)
+        model.add_count(count_psd)
+
+        # count everything that is in spine but not in psd
+        count_reg_spine_minus_psd = copy.deepcopy(base_count)
+        count_reg_spine_minus_psd.name = str(count_reg_spine_minus_psd.name + '_reg_spine_minus_psd') 
+        count_reg_spine_minus_psd.file_name = str('./react_data/seed_' + str(seed).zfill(5) + '/' + count_reg_spine_minus_psd.name + '.dat')
+        count_reg_spine_minus_psd.every_n_timesteps = sampling_periodicity
+        set_region_to_all_count_term_leaf_nodes(count_reg_spine_minus_psd.expression, spine - psd)
+        model.add_count(count_reg_spine_minus_psd)
+
+
 MCELL_PATH=os.environ.get('MCELL_PATH','')
+print(MCELL_PATH)
 if MCELL_PATH:
     lib_path=os.path.join(MCELL_PATH,'lib')
     sys.path.append(lib_path)
-   # print('yessuh')
+    print('Yay!')
 else:
     print("Error")
     sys.exit(1)
 
 import mcell as m 
-for x in range(20):
-        # viz_output = m.VizOutput(
-        #     output_files_prefix = './camkii_final1/viz_data/seed_00001/Scene',
-# )
-        # random.seed(x)
-    box_vertex_list = [
-            [ 0.025000000372529, -0.25, -0.25 ],
-            [ 0.025000000372529, 0.25, -0.25 ],
-            [ 0.524999976158142, 0.25, 0.25 ],
-            [ 0.524999976158142, 0.25, -0.25 ],
-            [ 0.025000000372529, 0.25, 0.25 ],
-            [ 0.524999976158142, -0.25, 0.25 ],
-            [ 0.025000000372529, -0.25, 0.25 ],
-            [ 0.524999976158142, -0.25, -0.25 ]
-        
-        ] 
+if __name__ == "__main__":
 
-    box_wall_list = [
-            # [1, 2, 0] defines a triangle connecting vertices 
-            # [-0.1, -0.1, 0.1], [-0.1, 0.1, -0.1], and
-            # [-0.1, -0.1, -0.1]
-            [ 7, 0, 3 ],
-            [ 3, 0, 1 ],
-            [ 4, 1, 6 ],
-            [ 0, 6, 1 ],
-            [ 1, 4, 3 ],
-            [ 2, 3, 4 ],
-            [ 7, 3, 5 ],
-            [ 2, 5, 3 ],
-            [ 6, 5, 4 ],
-            [ 2, 4, 5 ],
-            [ 5, 0, 7 ],
-            [ 6, 0, 5 ]
-        ] 
-    postsynaptic_PSD_wall_indices = [
-            2, 3
-        ]
-    postsynaptic_PSD = m.SurfaceRegion(
-            name = 'PSD',
-            wall_indices = postsynaptic_PSD_wall_indices
-        )
 
-    box_vertex_list2 = [
-            [ 0.0875197052955627, -0.150000005960464, -0.150000005960464 ],
-            [ 0.0875197052955627, -0.150000005960464, 0.150000005960464 ],
-            [ 0.0875197052955627, 0.150000005960464, -0.150000005960464 ],
-            [ 0.0875197052955627, 0.150000005960464, 0.150000005960464 ],
-            [ 0.387519717216492, -0.150000005960464, -0.150000005960464 ],
-            [ 0.387519717216492, -0.150000005960464, 0.150000005960464 ],
-            [ 0.387519717216492, 0.150000005960464, -0.150000005960464 ],
-            [ 0.387519717216492, 0.150000005960464, 0.150000005960464 ]
-        
-        ] 
-
-    box_wall_list2 = [
-            # [1, 2, 0] defines a triangle connecting vertices 
-            # [-0.1, -0.1, 0.1], [-0.1, 0.1, -0.1], and
-            # [-0.1, -0.1, -0.1]
-            [ 0, 1, 3 ],
-            [ 2, 3, 7 ],
-            [ 6, 7, 5 ],
-            [ 4, 5, 1 ],
-            [ 2, 6, 4 ],
-            [ 7, 3, 1 ]
-            
-        ] 
+    SEED = 2
+    n_pulses = 1 #get_parameter_value(n_pulses)
+    dt = 0.0001
     
+    V_true = 0.018822e-15
+    NA = 6.022e23/1e6 #1e-6
 
-    box = m.GeometryObject(
-            name = 'box',
-            vertex_list = box_vertex_list,
-            wall_list = box_wall_list,
-            surface_regions = [postsynaptic_PSD]
-        )
-    box2 =m.GeometryObject(
-            name = 'box2',
-            vertex_list = box_vertex_list2,
-            wall_list = box_wall_list2,
-        ) 
-    Ca_pat = m.ReleasePattern(
-            name = 'Ca_pat',
-            release_interval = 100e-3,
-            train_duration = 160e-3,
-            train_interval=0.5,
-            number_of_trains = 1
-        )
-    Ca_waveform = m.ReactionRule(
-    name = 'Ca_waveform',
-    reactants = [ m.Complex('Ca') ],
-    products = [ ],
-    fwd_rate = 50
-    )
-    trans_a = m.SurfaceClass(
-    name = 'trans_a',
-    type = m.SurfacePropertyType.TRANSPARENT,
-    affected_complex_pattern = m.AllMolecules
-    )
-    # box2.surface_class = trans_a
-    # box.is_bngl_compartment = True
-    # box.surface_compartment_name='O1M'
-    release_site_a = m.ReleaseSite(
-            name = 'rel_a', 
-            complex = m.Complex('CaM(C~0,N~0,CaMKII)'),# compartment_name='box'),
-            region=box2,
-            #location=(0,0,0),
-            number_to_release = 600      #change
-        )
-    release_site_b = m.ReleaseSite(
-            name = 'rel_b', 
-            complex = m.Complex('Ca()'),#compartment_name='box'), 
-            release_pattern=Ca_pat,
-            region=box, 
-            number_to_release =4320         #change
-        )
-    release_site_c = m.ReleaseSite(
-            name = 'rel_c', 
-            complex = m.Complex('CaMKII(r!1,l!6,T286~U,CaM).CaMKII(r!2,l!1,T286~U,CaM).CaMKII(r!3,l!2,T286~U,CaM).CaMKII(r!4,l!3,T286~U,CaM).CaMKII(r!5,l!4,T286~U,CaM).CaMKII(r!6,l!5,T286~U,CaM)'),# compartment_name='box'),
-            region=box2,
-            #location=(0,0,0),
-            number_to_release = 172        #change
-        )
+
+    tauR = 0.002 #time constant for Ca decay
+    tauF = 0.01
+
+    t1 = 0
+    t0 = 0.01
+    x = np.array(np.arange(0,0.08,dt))
+    alpha = 2000*(NA*V_true)*((x-t1)/tauR)*np.exp(-(x-t1)/tauF)*dt
+    params = m.bngl_utils.load_bngl_parameters('ryr2_CaM.bngl')
+    TIME_STEP = 1e-6
+    ITERATIONS = int(params['ITERATIONS'])
+                      
+    species_dict = dict([
+    ("Ca", NA*V_true*0.1),
+    ("CaM(C~0,N~0,ryr2)", NA*V_true*4),#NA*V_true*30,
+    ("RyR2(r!1,l!4,Ca~0,cam,S~0).RyR2(r!2,l!1,Ca~0,cam,S~0).RyR2(r!3,l!2,Ca~0,cam,S~0).RyR2(r!4,l!3,Ca~0,cam,S~0)", 4)])
+    seed = 10
     
-    SEED=random.randint(1,10000)
+    import spine133_geometry as geometry
     model = m.Model()
+
+
+    spine = geometry.d000p_sp133_cut
+    model.add_geometry_object(spine)
+    PSD = geometry.d000p_sp133_PSD
+    model.add_geometry_object(PSD)
+   
+    
+
+
+
+    for name in species_dict.keys():
+        print(name)
+        rel_site = m.ReleaseSite(
+            name = 'Release_of_' + name,
+            complex = m.Complex(name),
+            region = spine,
+            number_to_release = (int)(species_dict[name])
+        )
+        model.add_release_site(rel_site)
+    model.load_bngl("ryr2_CaM.bngl",'./react_data_wm/seed_' + str(SEED).zfill(5) + '/', spine)
+    for c in model.counts:
+        c.every_n_timesteps = ITERATIONS/100000
+    # for rel in range(len(x)):
+    #     if alpha[rel] > 0:
+    #         new_rel = m.ReleaseSite(
+    #             name = 'rel_%i' % rel,
+    #             complex = m.Complex('Ca'),
+    #             ## This location for spine048
+    #             #location = [0.009,-0.045,-0.1],
+    #             ## This location for spine133
+    #             location = [5.868,5.62319,5.88514],
+    #             number_to_release =alpha[rel],
+    #             release_time = t0+x[rel]
+    #     )
+    #         model.add_release_site(new_rel)
+
+    tp = m.SurfaceClass(
+        name = 'transparent',
+        type = m.SurfacePropertyType.TRANSPARENT,
+        affected_complex_pattern = m.AllMolecules
+    )
+
+    model.add_surface_class(tp)
+    PSD.surface_class = tp
+    
+
+    model.config.time_step = TIME_STEP
     model.config.seed = SEED
-    model.add_reaction_rule(Ca_waveform)
-        # model.add_viz_output(viz_output)  
-    model.add_geometry_object(box)
-    model.add_geometry_object(box2)
-    model.add_surface_class(trans_a)
-    model.add_release_site(release_site_a)
-    model.add_release_site(release_site_b)
-    model.add_release_site(release_site_c)
+    model.config.total_iterations = ITERATIONS
+    model.config.partition_dimension = 20#1
+    model.config.subpartition_dimension = 0.05
 
+    load_bngl_observables_and_create_psd_and_spine_variants(
+        model, 'ryr2_CaM.bngl', spine, PSD, ITERATIONS/100000, SEED) 
 
-
-    MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
-
-
-    model.load_bngl(
-            file_name = os.path.join(MODEL_PATH, 'ryr2_CaM.bngl'),
-            observables_path_or_file = './camkii/100ms6/seed_0000{}/'.format(x)
-        )    
-    ITERATIONS = 150000
-    model.config.total_iterations = ITERATIONS 
+    viz_output = model.VizOutput(
+        mode = m.VizMode.ASCII,
+        output_files_prefix = './viz_data/seed_' + str(SEED).zfill(5) + '/Scene',
+        every_n_timesteps = SAMPLING_PERIODICITY
+    )
 
     model.initialize()
-        # model.export_viz_data_model()
+
+    model.export_data_model()
     model.run_iterations(ITERATIONS)
     model.end_simulation()
-
-
-
-
-
-
-
-
-
-
